03c226e51497b14324c19f01b60bb2ccf87b55db
jay-java <jayprajapati.tops@gmail.com>
2024-03-14 12:40:50 +0530

    abstract encapsulation interface

--- CoreJAVA/src/core/P012_Abstraction.java
+++ CoreJAVA/src/core/P012_Abstraction.java
@@ -1,22 +1,55 @@
 package core;
-abstract class RBI{
-	abstract public void interest();
+
+abstract class RBI {
+	abstract public void interest();//function declaration
 	abstract public void HL();
-	public void repoRate() {
+	public static void repoRate() {
+		System.out.println("reporate : +-4%");
+	}
+	public void call() {
 		
 	}
 }
-class SBI extends RBI{
+class xyz{
 	
 }
-class PNB extends RBI{
-	
+class SBI extends RBI {
+	public void interest() {
+		System.out.println("SBI interest : 7%");
+	}
+	public void HL() {
+		System.out.println("SBI HL : 8%");
+	}
 }
-class JAVA extends RBI{
-	
+class PNB extends RBI {
+	public void interest() {
+		System.out.println("PNB interest : 8%");
+	}
+	public void HL() {
+		System.out.println("SBI HL : 9%");
+	}
+}
+class JAVA extends RBI {
+	public void interest() {
+		System.out.println("JAVA interest : 9%");
+	}
+	public void HL() {
+		System.out.println("JAVA HL : 10%");
+	}
 }
 public class P012_Abstraction {
 	public static void main(String[] args) {
-		
+		SBI s = new SBI();
+		s.interest();
+		s.HL();
+		SBI.repoRate();
+		PNB p = new PNB();
+		p.interest();
+		p.HL();
+		PNB.repoRate();
+		JAVA j = new JAVA();
+		j.interest();
+		j.HL();
+		JAVA.repoRate();
 	}
 }
