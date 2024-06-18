5e0beaa395ba53cdbb6af35b6686b27d11945053
jay-java <jayprajapati.tops@gmail.com>
2024-06-18 13:28:23 +0530

    spring MVC insert

--- SpringMVCNew/src/main/java/com/controller/UserController.java
+++ SpringMVCNew/src/main/java/com/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.dao.UserDao;
+import com.model.User;
 
 @Controller
 public class UserController {
@@ -24,6 +32,24 @@ public class UserController {
 	@RequestMapping("/register")
 	public String registerPage() {
 		return "register";
+	}
+//	@RequestMapping(value="/create",method = RequestMethod.GET)
+//	public void createAccount(
+//			@RequestParam("name") String name,
+//			@RequestParam("contact") long contact,
+//			@RequestParam("address") String address
+//			
+//			) {
+//		User u = new User();
+//		u.setName(name);
+//		u.setContact(contact);
+//		u.setAddress(address);
+//		System.out.println(u);
+//	}
+//	
+	@RequestMapping(value="/create",method = RequestMethod.GET)
+	public void createAccount(@ModelAttribute User u) {
+		dao.inserOrupdateUser(u);
 	}
 	
 }
--- SpringMVCNew/src/main/java/com/dao/UserDao.java
+++ SpringMVCNew/src/main/java/com/dao/UserDao.java
@@ -1,15 +1,14 @@
 package com.dao;
 
 import java.util.List;
-
 import javax.transaction.Transactional;
-
 import org.springframework.orm.hibernate5.HibernateTemplate;
-
+import org.springframework.stereotype.Component;
 import com.model.User;
 
+@Component
 public class UserDao {
-	
+
 	private HibernateTemplate hibernateTemplate;
 
 	public HibernateTemplate getHibernateTemplate() {
@@ -19,6 +18,7 @@ public class UserDao {
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
 		this.hibernateTemplate = hibernateTemplate;
 	}
+
 	@Transactional
 	public void inserOrupdateUser(User u) {
 		this.hibernateTemplate.saveOrUpdate(u);
@@ -29,12 +29,13 @@ public class UserDao {
 		User u = getUserById(id);
 		this.hibernateTemplate.delete(u);
 	}
+
 	public User getUserById(int id) {
 		return this.hibernateTemplate.get(User.class, id);
 	}
-	
-	public List<User> getAllUser(){
+
+	public List<User> getAllUser() {
 		return this.hibernateTemplate.loadAll(User.class);
 	}
-	
+
 }
